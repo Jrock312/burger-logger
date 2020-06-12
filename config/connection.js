eaf1bfa8224cb229f6fa4959a33eb7f5dddd27ed
Joel <guevarra_joel@yahoo.com>
2020-06-11 22:18:37 -0700

    redone application

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "pos@1932",
-  database: "burger_db"
+  password: "Pos@1932",
+  database: "burgers_db"
 });
 
-// Make connection.
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -18,5 +17,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
